4b7cae3229a3281a232d157ff2e707f4a9eccea3
tableonthewall <seydaozdemir07@gmail.com>
2021-12-06 11:09:01 +0300

    06/12/2021 find recipe

--- src/main/java/com/myapp/recipeapp/controller/RecipeController.java
+++ src/main/java/com/myapp/recipeapp/controller/RecipeController.java
@@ -5,11 +5,14 @@ import com.myapp.recipeapp.repository.RecipeRepository;
 import com.myapp.recipeapp.service.RecipeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/recipies")
 public class RecipeController {
     private final RecipeService recipeService;
 
@@ -17,8 +20,13 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping("/recipies")
+    @GetMapping("")
     public ResponseEntity<List<Recipe>> getAll(){
         return ResponseEntity.ok(recipeService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Recipe> findRecipe(@PathVariable Long id){
+        return ResponseEntity.ok(recipeService.findById(id));
     }
 }
