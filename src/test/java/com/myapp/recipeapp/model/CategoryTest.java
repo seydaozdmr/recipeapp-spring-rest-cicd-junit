c7df32dde773ea15fea004a631c1c720785811da
tableonthewall <seydaozdemir07@gmail.com>
2021-12-17 13:46:02 +0300

    17/12/2021 reactive test

--- src/test/java/com/myapp/recipeapp/model/CategoryTest.java
+++ src/test/java/com/myapp/recipeapp/model/CategoryTest.java
@@ -2,15 +2,43 @@ package com.myapp.recipeapp.model;
 
 import org.junit.Before;
 import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.*;
 
 public class CategoryTest {
 
     Category category;
+    List<Category> categoryList;
     @Before
     public void setUp() throws Exception {
-        category=new Category();
+        category=new Category("American",null);
+        categoryList=List.of(category,new Category("Italian",null),new Category("Turkey",null));
+    }
+
+    @Test
+    public void monoTest() throws InterruptedException {
+        CountDownLatch countDownLatch=new CountDownLatch(1);
+        Mono<Category> categoryMono=Mono.just(category);
+        categoryMono.delayElement(Duration.ofSeconds(3)).doOnTerminate(countDownLatch::countDown).subscribe(p-> System.out.println(p));
+
+        //Category category=categoryMono.block();
+        //Thread.currentThread().join();
+        //System.out.println(category.getDescription());
+    }
+
+    @Test
+    public void fluxTest() throws InterruptedException {
+        CountDownLatch countDownLatch=new CountDownLatch(1);
+        Flux<List<Category>> flux=Flux.just(categoryList);
+        flux.delayElements(Duration.ofSeconds(3)).doOnComplete(countDownLatch::countDown)
+                .subscribe(System.out::println);
+        countDownLatch.await();
     }
 
     @Test
